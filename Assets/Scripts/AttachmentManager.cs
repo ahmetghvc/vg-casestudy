5cc42767881aa23d73dc103fd1a345461da6bd6a
Ahmet Evsen <ahmetevsen@outlook.com>
2025-04-17 01:29:53 +0300

    Logic Enhanced SetActive Attachments

--- Assets/Scripts/AttachmentManager.cs
+++ Assets/Scripts/AttachmentManager.cs
@@ -46,116 +46,80 @@ public class AttachmentManager : MonoBehaviour
     bool x = false;
 
     //STATE
+    private Dictionary<string, GameObject[]> attachments;
 
 
-    //METHODS
+
+
+
+
+    //PROCESS
+    private Dictionary<string, GameObject[]> attachmentGroups;
+
+    void Start()
+    {
+        attachmentGroups = new Dictionary<string, GameObject[]>
+    {
+        { "scope1", scope1 },
+        { "scope2", scope2 },
+        { "scope3", scope3 },
+        { "mags1", mags1 },
+        { "mags2", mags2 },
+        { "mags3", mags3 },
+        { "mags4", mags4 },
+        { "mags5", mags5 },
+        { "barrels1", barrels1 },
+        { "barrels2", barrels2 },
+        { "barrels3", barrels3 },
+        { "tactical1", tactical1 },
+        { "tactical2", tactical2 },
+        { "stock1", stock1 },
+        { "stock2", stock2 }
+
+    };
+    }
 
 
     //INPUT
 
 
+    //METHODS
+    public void ToggleAttachment(string name)
+    {
+        if (attachmentGroups.TryGetValue(name, out GameObject[] objects))
+        {
+            foreach (GameObject go in objects)
+            {
+                if (go != null)
+                    go.SetActive(true);
+            }
+  
+        }
+
+    }
 
 
+    public void HideAttachment(string name)
+    {
+        if (attachmentGroups.TryGetValue(name, out GameObject[] objects))
+        {
+            foreach (GameObject go in objects)
+            {
+                if (go != null)
+                    go.SetActive(false);
+            }
+
+
+        }
+
+    }
 
 
     //DEBUG METHODS
 
-    IEnumerator Switcher()
-    {
 
-        
-        //yield return new WaitForEndOfFrame();
-        
-        yield return null;
-        x = !x;
 
-    }
 
-    [ContextMenu("Debug Scopes")]
-    void DebugIt()
-    {
-       
-        
-
-        foreach (GameObject item in scope1)
-        {
-            Debug.Log(item.name);
-        }
-
-    }
-
-    
-    public void HandleScope1()
-    {
-        
-        if (!x)
-        {
-            ShowScope1();
-        }
-        if (x)
-        {
-            HideScope1();
-        }
-
-        HideScope2 ();
-    }
-
-    public void HandleScope2()
-    {
-
-        if (!x)
-        {
-            ShowScope2();
-        }
-        if (x)
-        {
-            HideScope2();
-        }
-
-        HideScope1 ();
-    }
-
-    private void HideScope2()
-    {
-        foreach (GameObject item in scope2)
-        {
-            item.SetActive(false);
-        }
-
-        StartCoroutine(Switcher());
-    }
-
-    private void ShowScope2()
-    {
-        foreach (GameObject item in scope2)
-        {
-            item.SetActive(true);
-        }
-
-        StartCoroutine(Switcher());
-    }
-
-    private void HideScope1()
-    {
-        foreach (GameObject item in scope1)
-        {
-            item.SetActive(false);
-        }
-
-        StartCoroutine(Switcher());
-    }
-
-    private void ShowScope1()
-    {
-        foreach (GameObject item in scope1)
-        {
-            item.SetActive(true);
-        }
-
-        StartCoroutine(Switcher());
-    }
-
-    
 
 
 
